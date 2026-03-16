3795eda5647989519286750f7284260d12a5f534
Maxim Ermolaev <ermolaevm22@gmail.com>
2026-03-16 10:55:43 +0700

    Initial Commit

---
+++ template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by Максим Ермолаев on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ templateTests/templateTests.swift
@@ -0,0 +1,16 @@
+//
+//  templateTests.swift
+//  templateTests
+//
+//  Created by Максим Ермолаев on 16.03.2026.
+//
+
+import Testing
+
+struct templateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
